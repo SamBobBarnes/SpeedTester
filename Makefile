f136d2bb0dcda88dc6752e5c6b9659b4af7df7b4
SamBobBarnes <54638834+SamBobBarnes@users.noreply.github.com>
2023-11-04 21:42:39 -0500

    getting there

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ build:
 
 test:
 	cd SpeedtestContainer && docker build -t sambobbarnes/speedtest . && cd .. && docker compose up
+
+test-d:
+	cd SpeedtestContainer && docker build -t sambobbarnes/speedtest . && cd .. && docker compose up -d
--- SpeedtestContainer/speedtest.sh
+++ SpeedtestContainer/speedtest.sh
@@ -1,51 +1,53 @@
 
-# MQTT_HOST=${MQTT_HOST:-localhost}
-# MQTT_ID=${MQTT_ID:-speedtest2mqtt}
-# MQTT_TOPIC=${MQTT_TOPIC:-speedtest}
-# MQTT_OPTIONS=${MQTT_OPTIONS:-"-r"}
-# MQTT_USER=${MQTT_USER:-user}
-# MQTT_PASS=${MQTT_PASS:-pass}
+MQTT_HOST=${MQTT_HOST:-localhost}
+MQTT_ID=${MQTT_ID:-speedtest2mqtt}
+MQTT_TOPIC=${MQTT_TOPIC:-speedtest}
+MQTT_OPTIONS=${MQTT_OPTIONS:-"-r"}
+MQTT_USER=${MQTT_USER:-user}
+MQTT_PASS=${MQTT_PASS:-pass}
 
-# file=~/ookla.json
-echo "Speedtest has been started... "
-echo "$(date -Iseconds) starting speedtest"
+file=~/ookla.json
+echo "Speedtest has been started... \n"
+echo "$(date -Iseconds) starting speedtest \n"
 
-speedtest --accept-license -f json-pretty | jq
-# speedtest --accept-license --accept-gdpr -f json-pretty > ${file}
+speedtest --accept-license -f json-pretty > ${file}
+# speedtest --help
 
-# downraw=$(jq -r '.download.bandwidth' ${file})
-# download=$(printf %.2f\\n "$((downraw * 8))e-6")
-# upraw=$(jq -r '.upload.bandwidth' ${file})
-# upload=$(printf %.2f\\n "$((upraw * 8))e-6")
-# ping=$(jq -r '.ping.latency' ${file})
-# jitter=$(jq -r '.ping.jitter' ${file})
-# packetloss=$(jq -r '.packetLoss' ${file})
-# serverid=$(jq -r '.server.id' ${file})
-# servername=$(jq -r '.server.name' ${file})
-# servercountry=$(jq -r '.server.country' ${file})
-# serverlocation=$(jq -r '.server.location' ${file})
-# serverhost=$(jq -r '.server.host' ${file})
-# timestamp=$(jq -r '.timestamp' ${file})
+echo "Speedtest finished \n"
 
-# echo "$(date -Iseconds) speedtest results"
+downraw=$(jq -r '.download.bandwidth' ${file})
+download=$(printf %.2f\\n "$((downraw * 8))e-6")
+upraw=$(jq -r '.upload.bandwidth' ${file})
+upload=$(printf %.2f\\n "$((upraw * 8))e-6")
+ping=$(jq -r '.ping.latency' ${file})
+jitter=$(jq -r '.ping.jitter' ${file})
+packetloss=$(jq -r '.packetLoss' ${file})
+serverid=$(jq -r '.server.id' ${file})
+servername=$(jq -r '.server.name' ${file})
+servercountry=$(jq -r '.server.country' ${file})
+serverlocation=$(jq -r '.server.location' ${file})
+serverhost=$(jq -r '.server.host' ${file})
+timestamp=$(jq -r '.timestamp' ${file})
 
-# echo "$(date -Iseconds) download = ${download} Mbps"
-# echo "$(date -Iseconds) upload =  ${upload} Mbps"
-# echo "$(date -Iseconds) ping =  ${ping} ms"
-# echo "$(date -Iseconds) jitter = ${jitter} ms"
+echo "$(date -Iseconds) speedtest results"
 
-# echo "$(date -Iseconds) sending results to ${MQTT_HOST} as clientID ${MQTT_ID} with options ${MQTT_OPTIONS} using user ${MQTT_USER}"
+echo "$(date -Iseconds) download = ${download} Mbps"
+echo "$(date -Iseconds) upload =  ${upload} Mbps"
+echo "$(date -Iseconds) ping =  ${ping} ms"
+echo "$(date -Iseconds) jitter = ${jitter} ms"
 
-# /usr/bin/mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/download -m "${download}"
-# /usr/bin/mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/upload -m "${upload}"
-# /usr/bin/mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/ping -m "${ping}"
-# /usr/bin/mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/jitter -m "${jitter}"
-# /usr/bin/mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/packetloss -m "${packetloss}"
+echo "$(date -Iseconds) sending results to ${MQTT_HOST} as clientID ${MQTT_ID} with options ${MQTT_OPTIONS} using user ${MQTT_USER}"
 
-# /usr/bin/mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/server/id -m "${serverid}"
-# /usr/bin/mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/server/name -m "${servername}"
-# /usr/bin/mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/server/location -m "${serverlocation}"
-# /usr/bin/mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/server/host -m "${serverhost}"
-# /usr/bin/mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/server/country -m "${servercountry}"
+mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/download -m "${download}"
+mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/upload -m "${upload}"
+mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/ping -m "${ping}"
+mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/jitter -m "${jitter}"
+mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/packetloss -m "${packetloss}"
 
-# /usr/bin/mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/timestamp -m "${timestamp}"
+mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/server/id -m "${serverid}"
+mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/server/name -m "${servername}"
+mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/server/location -m "${serverlocation}"
+mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/server/host -m "${serverhost}"
+mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/server/country -m "${servercountry}"
+
+mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/timestamp -m "${timestamp}"
